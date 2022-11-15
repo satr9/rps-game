8cdf797d601d8050c067980c268e0b4ff26b83d7
satr9 <110054210+satr9@users.noreply.github.com>
2022-11-15 21:00:35 +0400

    Create rng function for computer choice

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ let scissors = 3;
 
 // create the hard coded value for the player
 let playerChoice = 1;
+
+// create the function that gets a random number between 1-3 inc to represent computer choice.
+// Use Math.random with Math.floor
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    return computerChoice;
+}

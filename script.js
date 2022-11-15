561e48322d6509d1602fa8b40a1516e5ea46db83
satr9 <110054210+satr9@users.noreply.github.com>
2022-11-15 21:24:27 +0400

    Create function that plays one round of the game
    The function is a long chain of if...else if statements that has a
    strict equality check all over the place to assign a string explaining
    the result in every case of the condition. Then it returns that final
    result.

--- script.js
+++ script.js
@@ -4,11 +4,36 @@ let paper = 2;
 let scissors = 3;
 
 // create the hard coded value for the player
-let playerChoice = 1;
+let player = 1;
 
-// create the function that gets a random number between 1-3 inc to represent computer choice.
-// Use Math.random with Math.floor
+// create the function that gets a random number between 1-3 inc to represent computer choice. Use Math.random with Math.floor
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
     return computerChoice;
+}
+
+// create a function that plays one round of the game with conditional statements
+function playRound() {
+    let computer = getComputerChoice();
+    let result = "";
+    if (computer === 1 && player === 1) {
+        result = "It's a tie. You both chose rock.";
+    } else if (computer === 1 && player === 2) {
+        result = "You win. Paper beats rock.";
+    } else if (computer === 1 && player === 3) {
+        result = "You lose. Rock beats scissors.";
+    } else if (computer === 2 && player === 1) {
+        result = "You lose. Paper beats rock.";
+    } else if (computer === 2 && player === 2) {
+        result = "It's a tie. You both chose paper.";
+    } else if (computer === 2 && player === 3) {
+        result = "You win. Scissors beats paper.";
+    } else if (computer === 3 && player === 1) {
+        result = "You win. Rock beats scissors.";
+    } else if (computer === 3 && player === 2) {
+        result = "You lose. Scissors beats paper.";
+    } else {
+        result = "It's a tie. You both chose scissors.";
+    }
+    return result;
 }

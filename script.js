0ecd125ddb0b6f2070ccf21746f700fd9aa811fb
satr9 <110054210+satr9@users.noreply.github.com>
2022-11-15 20:55:02 +0400

    Edit script to add initial variables
    Create the three variables that represent the three choices and the
    player's hard coded choice.

---
+++ script.js
@@ -0,0 +1,7 @@
+// create three variables with int values that stand for the three choices in the game
+let rock = 1;
+let paper = 2;
+let scissors = 3;
+
+// create the hard coded value for the player
+let playerChoice = 1;

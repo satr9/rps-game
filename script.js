70a1d8c3c2cf346a1703f76e85c9a622679ceeae
satr9 <110054210+satr9@users.noreply.github.com>
2022-11-16 17:08:59 +0400

    Edit script for cleaner code and remove notes
    Clean up the script. It still does the same thing but now the code is
    cleaner, with better implementation, using actual strings for choice
    values instead of numbers, making the user's string capitalized and
    having clearer function structure. Also deleted notes as it wasn't
    needed.

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
-// create three variables with int values that stand for the three choices in the game
-let rock = 1;
-let paper = 2;
-let scissors = 3;
-
-// create the hard coded value for the player
-let player = 1;
-
-// create the function that gets a random number between 1-3 inc to represent computer choice. Use Math.random with Math.floor
+// create the function that gets a random number between 1-3 inc to represent computer choice. Use Math.random with Math.floor. Assign 1 to the string 'rock', 2 to 'paper', 3 to 'scissors'.
 function getComputerChoice() {
+    let result = "";
     let computerChoice = Math.floor(Math.random() * 3) + 1;
-    return computerChoice;
+    if (computerChoice === 1) {
+        result = "Rock";
+    } else if (computerChoice === 2) {
+        result = "Paper";
+    } else if (computerChoice === 3) {
+        result = "Scissors";
+    }
+    return result;
+}
+
+// get the player's choice and make it case insensitive, then return the choice as a capitalized string.
+function getUserChoice() {
+    let userChoice = prompt("What do you pick?");
+    let lower = userChoice.toLocaleLowerCase();
+    let initial = lower[0].toUpperCase();
+    return initial + lower.slice(1);
 }
 
 // create a function that plays one round of the game with conditional statements
 function playRound() {
+    let user = getUserChoice();
     let computer = getComputerChoice();
-    let result = "";
-    if (computer === 1 && player === 1) {
-        result = "It's a tie. You both chose rock.";
-    } else if (computer === 1 && player === 2) {
-        result = "You win. Paper beats rock.";
-    } else if (computer === 1 && player === 3) {
-        result = "You lose. Rock beats scissors.";
-    } else if (computer === 2 && player === 1) {
-        result = "You lose. Paper beats rock.";
-    } else if (computer === 2 && player === 2) {
-        result = "It's a tie. You both chose paper.";
-    } else if (computer === 2 && player === 3) {
-        result = "You win. Scissors beats paper.";
-    } else if (computer === 3 && player === 1) {
-        result = "You win. Rock beats scissors.";
-    } else if (computer === 3 && player === 2) {
-        result = "You lose. Scissors beats paper.";
-    } else {
-        result = "It's a tie. You both chose scissors.";
+    if (user === "Rock" && computer === "Rock") {
+        return "It's a tie. You both chose rock.";
+    } else if (user === "Rock" && computer === "Paper") {
+        return "Computer wins. Paper beats rock.";
+    } else if (user === "Rock" && computer === "Scissors") {
+        return "You win! Rock beats scissors.";
+    } else if (user === "Paper" && computer === "Rock") {
+        return "You win! Paper beats rock.";
+    } else if (user === "Paper" && computer === "Paper") {
+        return "It's a tie. You both chose paper.";
+    } else if (user === "Paper" && computer === "Scissors") {
+        return "Computer wins. Scissors beats paper.";
+    } else if (user === "Scissors" && computer === "Rock") {
+        return "Computer wins. Rock beats scissors.";
+    } else if (user === "Scissors" && computer === "Paper") {
+        return "You win! Scissors beats paper.";
+    } else if (user === "Scissors" && computer === "Scissors") {
+        return "It's a tie. You both chose scissors.";
     }
-    return result;
 }

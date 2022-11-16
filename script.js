caa633f370cd2e716c4813d935c725c544cdef93
satr9 <110054210+satr9@users.noreply.github.com>
2022-11-16 18:23:56 +0400

    Add game function which plays five rounds
    Create a function that plays five rounds of the game which could be
    customized to include arbitrary number of rounds. The problem is that
    this now plays for five rounds exactly, instead of playing until first
    player to reach five points.

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function playRound() {
     if (user === "Rock" && computer === "Rock") {
         return "It's a tie. You both chose rock.";
     } else if (user === "Rock" && computer === "Paper") {
-        return "Computer wins. Paper beats rock.";
+        return "You lose. Paper beats rock.";
     } else if (user === "Rock" && computer === "Scissors") {
         return "You win! Rock beats scissors.";
     } else if (user === "Paper" && computer === "Rock") {
@@ -35,12 +35,40 @@ function playRound() {
     } else if (user === "Paper" && computer === "Paper") {
         return "It's a tie. You both chose paper.";
     } else if (user === "Paper" && computer === "Scissors") {
-        return "Computer wins. Scissors beats paper.";
+        return "You lose. Scissors beats paper.";
     } else if (user === "Scissors" && computer === "Rock") {
-        return "Computer wins. Rock beats scissors.";
+        return "You lose. Rock beats scissors.";
     } else if (user === "Scissors" && computer === "Paper") {
         return "You win! Scissors beats paper.";
     } else if (user === "Scissors" && computer === "Scissors") {
         return "It's a tie. You both chose scissors.";
     }
 }
+
+// create a new function that plays a certain number of rounds of the game using for loops
+function game() {
+    let userScore = 0;
+    let computerScore = 0;
+    let round;
+    for (let i = 1; i < 6; i++) {
+        // to keep score, each iteration it checks if the playRound function returns the string "win" and then adds one to userScore, else adds one to computerScore. If it's a tie, no score is added. The game function returns a string summing up the entire game with all the relevant scores displayed using a conditional statement.
+        console.log(`round ${i}`);
+        round = playRound();
+        console.log(round);
+        if (round.includes("win")) {
+            userScore++;
+        } else if (round.includes("lose")) {
+            computerScore++;
+        }
+        console.log(`Player: ${userScore} - Computer: ${computerScore}`);
+    }
+    let result =
+        userScore === 5
+            ? "Congratulations, you win the game!"
+            : "Sorry, computer wins. Better luck next time...";
+    return result;
+}
+
+game();
+
+// this works but only for ***five rounds of actual play*** instead of first player reaching five points...
